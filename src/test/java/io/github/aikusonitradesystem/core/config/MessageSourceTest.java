99ee5b0a94e7a7d99704d715f4bdfd73ba36780b
HoonKi Kim <31752803+aikusoni@users.noreply.github.com>
2024-09-07 17:44:03 +0900

    [feat/MessageSource:] * 메시지 네이밍 룰에 맞춰 메시지 변경

--- src/test/java/io/github/aikusonitradesystem/core/config/MessageSourceTest.java
+++ src/test/java/io/github/aikusonitradesystem/core/config/MessageSourceTest.java
@@ -17,20 +17,20 @@ public class MessageSourceTest {
 
     @Test
     public void testMessageInEnglish() {
-        String message = messageSource.getMessage("ats-spring-core.test", null, Locale.ENGLISH);
+        String message = messageSource.getMessage("core.test", null, Locale.ENGLISH);
         assertEquals("This is a test message of the core module.", message);
     }
 
     @Test
     public void testMessageInKorean() {
-        String message = messageSource.getMessage("ats-spring-core.test", null, Locale.KOREAN);
+        String message = messageSource.getMessage("core.test", null, Locale.KOREAN);
         assertEquals("이 메시지는 코어 모듈의 메시지 테스트를 위한 메시지입니다.", message);
     }
 
     @Test
     public void testMessageWithDefault() {
         // 메시지가 없는 경우 기본 메시지 사용
-        String message = messageSource.getMessage("ats-spring-core.test.default", null, Locale.ENGLISH);
+        String message = messageSource.getMessage("core.test.default", null, Locale.ENGLISH);
         assertEquals("This is a message to test if the default message is fetched correctly.", message);
     }
 }
